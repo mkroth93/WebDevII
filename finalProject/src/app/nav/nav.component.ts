7a890768543c257978d06c8292f036783730b3d3
mkroth93 <46752738+mkroth93@users.noreply.github.com>
2019-12-08 13:12:09 -0600

    updated css

--- finalProject/src/app/nav/nav.component.ts
+++ finalProject/src/app/nav/nav.component.ts
@@ -3,7 +3,6 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'pm-nav',
     templateUrl: './nav.component.html',
-    styleUrls: ['./nav.component.css']
 })
 
 export class NavComponent{
